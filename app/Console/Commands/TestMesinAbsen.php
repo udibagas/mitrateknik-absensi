564b2caad95c99d90fcf3e2466f1214f8815320d
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-07 20:47:52 +0700

    add test mesin

---
+++ app/Console/Commands/TestMesinAbsen.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use ZKLibrary;
+
+class TestMesinAbsen extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'absen:test {ip}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test ambil data log mesin absen';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $ip = $this->argument('ip');
+        $this->info($ip);
+
+        $zk = new ZKLibrary($ip, 4370);
+        $zk->connect();
+        $logs = $zk->getAttendance();
+        $this->line(json_encode($logs));
+        $zk->disconnect();
+    }
+}
