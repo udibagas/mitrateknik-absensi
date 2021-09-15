125f0d4c9bec6cab165e49362d139db5166104dc
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-16 02:01:24 +0700

    build

--- frontend/plugins/echo.js
+++ frontend/plugins/echo.js
@@ -4,8 +4,8 @@ window.Pusher = require('pusher-js')
 
 window.Echo = new Echo({
   broadcaster: 'pusher',
-  key: 'pusher_key_123',
-  // wsHost: '10.4.21.112',
+  // key: 'pusher_key_123',
+  wsHost: '10.4.21.112',
   wsHost: '127.0.0.1',
   wsPort: 6001,
   forceTLS: false,
