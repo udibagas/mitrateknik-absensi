726adcbb3df68adca3128badbf8c1aa9a45652dd
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-15 23:11:01 +0700

    update

---
+++ public/_nuxt/34e66de.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{584:function(t,n,e){"use strict";e.r(n);var l={layout:"default",data:function(){return{popup:!1,attendance:{}}},methods:{}},o=e(35),component=Object(o.a)(l,(function(){var t=this,n=t.$createElement;return(t._self._c||n)("div",[t._v("TODO")])}),[],!1,null,null,null);n.default=component.exports}}]);
