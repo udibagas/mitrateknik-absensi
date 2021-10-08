be533faca9a4daa71e0af70a594a66b879e0e1e6
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-10-08 09:51:54 +0700

    update

--- app/Console/Commands/KoreksiAbsensi.php
+++ app/Console/Commands/KoreksiAbsensi.php
@@ -13,7 +13,7 @@ class KoreksiAbsensi extends Command
      *
      * @var string
      */
-    protected $signature = 'absensi:koreksi {pin} {date}';
+    protected $signature = 'absensi:koreksi {date} {pin?}';
 
     /**
      * The console command description.
@@ -39,64 +39,71 @@ class KoreksiAbsensi extends Command
      */
     public function handle()
     {
-        $attendance = Attendance::where('pin', $this->argument('pin'))
-            ->where('date', $this->argument('date'))
-            ->first();
+        $attendances = Attendance::when($this->argument('pin'), function ($q) {
+            $q->where('pin', $this->argument('pin'));
+        })->where('date', $this->argument('date'))->get();
 
-        if (!$attendance) {
+        if (!$attendances) {
             $this->error('Data absensi tidak ditemukan');
         }
 
-        $this->line('INITIAL DATA:');
-        $this->table(
-            ['Parameter', 'Value'],
-            [
-                ['PIN', $attendance->pin],
-                ['Name', $attendance->name],
-                ['First In', $attendance->first_in],
-                ['Last Out', $attendance->last_out],
-                ['Rest Start', $attendance->rest_start],
-                ['Rest end', $attendance->rest_end],
-                ['Rest Duration', $attendance->rest_duration],
-                ['Work Duration', $attendance->work_duration],
-                ['Prosentase', $attendance->prosentase . '%']
-            ],
-        );
+        foreach ($attendances as $attendance) {
+            $this->line('INITIAL DATA:');
+            $this->table(
+                ['Parameter', 'Value'],
+                [
+                    ['PIN', $attendance->pin],
+                    ['Name', $attendance->name],
+                    ['First In', $attendance->first_in],
+                    ['Last Out', $attendance->last_out],
+                    ['Rest Start', $attendance->rest_start],
+                    ['Rest end', $attendance->rest_end],
+                    ['Rest Duration', $attendance->rest_duration],
+                    ['Work Duration', $attendance->work_duration],
+                    ['Prosentase', $attendance->prosentase . '%']
+                ],
+            );
 
-        $access = Access::where('pin', $this->argument('pin'))
-            ->whereDate('event_time', $this->argument('date'))
-            ->get()->map(function ($i) {
-                return [$i->event_time_time, $i->event_point_name];
-            })->toArray();
+            $access = Access::where('pin', $this->argument('pin'))
+                ->whereDate('event_time', $this->argument('date'))
+                ->get()->map(function ($i) {
+                    return [$i->event_time_time, $i->event_point_name];
+                })->toArray();
 
-        if (!$access) {
-            $this->error('Data akses tidak ditemukan');
+            if (count($access) == 0) {
+                $this->error('Data akses tidak ditemukan');
+                continue;
+            }
+
+            $this->line('LOG ACCESS:');
+            $this->table(['Time', 'Gate'], $access);
+
+            // TODO: update jam istirahat juga
+            $attendance->update([
+                'first_in' => $access[0][0],
+                'last_out' => end($access)[0],
+            ]);
+            $attendance->refresh();
+
+            $this->line('CORRECTIVE DATA:');
+
+            $this->table(
+                ['Parameter', 'Value'],
+                [
+                    ['PIN', $attendance->pin],
+                    ['Name', $attendance->name],
+                    ['First In', $attendance->first_in],
+                    ['Last Out', $attendance->last_out],
+                    ['Rest Start', $attendance->rest_start],
+                    ['Rest end', $attendance->rest_end],
+                    ['Rest Duration', $attendance->rest_duration],
+                    ['Work Duration', $attendance->work_duration],
+                    ['Prosentase', $attendance->prosentase . '%']
+                ],
+            );
+
+            $this->info('Data berhasil diupdate');
+            $this->line('=============================================');
         }
-
-        $this->line('LOG ACCESS:');
-        $this->table(['Time', 'Gate'], $access);
-
-        $attendance->update([
-            'first_in' => $access[0][0],
-            'last_out' => end($access)[0],
-        ]);
-        $attendance->refresh();
-
-        $this->line('CORRECTIVE DATA:');
-
-        $this->table(
-            ['Parameter', 'Value'],
-            [
-                ['PIN', $attendance->pin],
-                ['Name', $attendance->name],
-                ['First In', $attendance->first_in],
-                ['Last Out', $attendance->last_out],
-                ['Rest Start', $attendance->rest_start],
-                ['Rest end', $attendance->rest_end],
-                ['Rest Duration', $attendance->rest_duration],
-                ['Work Duration', $attendance->work_duration],
-                ['Prosentase', $attendance->prosentase . '%']
-            ],
-        );
     }
 }
